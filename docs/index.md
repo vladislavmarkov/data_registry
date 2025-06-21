55cea1635fd3cc2f7004cfa9a316664fa83b67e6
Vladyslav Markov <vladislav.u.markov@gmail.com>
2025-06-21 23:53:40 +0200

    rework r/o, r/w, and add contexts

--- docs/index.md
+++ docs/index.md
@@ -8,20 +8,9 @@ The core idea is to define data *entries* identified by a *tag*, a data type,
 and, optionally, *read* and *write* accessors.
 
 ```cpp
-_e(tag, type);
-```
-
-or
-
-```cpp
-_e(tag, type, reader, writer);
-```
-
-In the near future, *contexts* will be supported for both *readers* and
-*writers*.
-
-```cpp
-_e(tag, type, reader, reader_ctx, writer, writer_ctx);
+_e(tag, type);                 // static
+_e(tag, type, reader);         // reader
+_e(tag, type, reader, writer); // reader & writer
 ```
 
 ## Statics
@@ -33,10 +22,14 @@ Example:
 ```cpp
 // .h
 _e(speed, unsigned short);
+```
 
+```cpp
 // .cpp
 _store_e(speed, unsigned short);
+```
 
+```cpp
 // usage
 void override_speed()
 {
@@ -66,7 +59,9 @@ Example:
 ```cpp
 // .h
 _e(speed, uint16_t, get_speed);
+```
 
+```cpp
 // usage
 int foo()
 {
@@ -79,3 +74,113 @@ int foo()
     We don’t need to store the data in any translation unit with the
     *_store_e()* macro, because the *reader* (*get_speed()*) defines the data
     and is responsible for retrieving it from its source.
+
+## Contexts
+
+Sometimes, user needs to pass additional data while reading or writing the
+value of an entry. It can be a single variable, or the whole set of them.
+Let's say we need to read and write to the entry value in a thread-safe manner.
+In such case we can pass a *synchronization primitive* as a context to both
+reader and writer. The best way to do this is to pass `std::shared_mutex`
+(C++17), or `boost::shared_mutex` (C++14) as a context to both accessor functions.
+
+Example:
+
+```cpp
+// .h
+
+// reads current temperature
+float get_temperature(std::shared_mutex& mx);
+
+// sets the temperature to desirable value
+void set_temperature(float temp, std::shared_mutex& mx);
+```
+
+```cpp
+// .cpp - possible implementation of accessors
+float get_temperature(std::shared_mutex& mx)
+{
+    std::shared_lock lk(mx);
+    return read_temperature();
+}
+
+void set_temperature(float temp, std::shared_mutex& mx)
+{
+    std::unique_lock lk(mx);
+    set_pref_temperature(temp);
+}
+
+_e(temperature, float, get_temperature, set_temperature);
+```
+
+```cpp
+// usage
+
+static std::shared_mutex g_mx;
+
+void manipulate_temperature()
+{
+    // ...
+
+    std::cout << "temperature before " << reg::get<temperature>(g_mx) << '\n';
+
+    reg::set<temperature>(20, g_mx);
+
+    std::cout << "temperature after " << reg::get<temperature>(g_mx) << '\n';
+
+    // ...
+}
+```
+
+Keep in mind, that it's also possible to pass multiple contexts, in case if
+user-defined readers and (or) writers have corresponding function parameters.
+
+Example:
+
+```cpp
+// .h
+enum class room_t : unsigned char {
+    bathroom,
+    bedroom,
+    kitchen,
+    livingroom
+};
+
+// let's say we have a reader function for a temperature for different
+// appartments of a big appartment complex
+float get_room_temperature(
+    std::shared_mutex& mx,
+    unsigned int appartment_no,
+    room_t room);
+
+_e(temperature, float, get_room_temperature);
+```
+
+```cpp
+// .cpp
+float get_room_temperature(
+    std::shared_mutex& mx,
+    unsigned int appartment_no,
+    room_t room)
+{
+    std::shared_lock lk(mx);
+    return read_sensor_data(appartment_no, room);
+}
+```
+
+```cpp
+// usage
+static std::shared_mutex g_mx;
+
+void check_all_kitchens()
+{
+    // iterating through all appartment numbers
+    for (auto const apt_no : all_appartments)
+    {
+        if (reg::get<temperature>(g_mx, apt_no, room_t::kitchen) > 30.f)
+        {
+            notify_fire_dpt(apt_no);
+        }
+    }
+}
+```
--- include/data/registry.h
+++ include/data/registry.h
@@ -1,103 +1,116 @@
 #ifndef REG_DATA_REGISTRY_H
 #define REG_DATA_REGISTRY_H
 
-#include <tuple>
+#include <type_traits>
 #include <utility>
 
 namespace reg
 {
 
-namespace traits
+template<typename Tag, typename... Args>
+inline constexpr auto get(Args&&... args) -> decltype(Tag::get(args...))
 {
-
-template<typename T>
-struct function_traits;
-
-template<typename R, typename... Args>
-struct function_traits<R (*)(Args...)>
-{
-    using result_t       = R;
-    using argument_types = std::tuple<Args...>;
-
-    static constexpr std::size_t argsnum = sizeof...(Args);
-
-    template<std::size_t N>
-    struct argument
-    {
-        static_assert(N < argsnum, "Invalid argument index");
-        using type =
-            typename std::tuple_element_t<N, std::tuple<Args...>>::type;
-    };
-};
-
-} // namespace traits
-
-template<typename Tag>
-inline constexpr auto get() -> decltype(auto)
-{
-    return Tag::get();
+    return Tag::get(args...);
 }
 
-template<typename Tag, typename T>
-void set(T&& value)
+template<typename Tag, typename T, typename... Args>
+void set(T&& value, Args&&... args)
 {
-    Tag::set(std::forward<T>(value));
+    Tag::set(::std::forward<T>(value), ::std::forward<Args>(args)...);
 }
 
 } // namespace reg
 
 /*
- * 1. Fundamental statics are read by value.
- * 2. Everything else is read by const reference.
- * 3. Statics are r/w, since there're no other ways to modify them.
+ * Variations
+ *
+ * Core API:
+ *
+ *  1. _e(tag,   type)                 - static
+ *  2. _e(tag,   type, reader)         - r/o
+ *  3. _e(tag,   type, reader, writer) - r/w
+ *
+ *  Additional requirements:
+ *
+ *  1. It should be possible for readers/writers to be represented with either
+ *     raw function, or a functional object.
  */
-#define _e_static(Tag, Type)                                   \
-    struct Tag                                                 \
-    {                                                          \
-        using type = std::conditional_t<                       \
-            std::is_fundamental<Type>::value,                  \
-            Type,                                              \
-            Type const&>;                                      \
-                                                               \
-        static inline auto get() -> type { return _value; }    \
-        static inline void set(type value) { _value = value; } \
-                                                               \
-    private:                                                   \
-        static Type _value;                                    \
+
+namespace reg
+{
+
+namespace detail
+{
+
+template<typename T>
+using modify_type
+    = ::std::conditional_t<::std::is_fundamental<T>::value, T, T const&>;
+
+}
+
+} // namespace reg
+
+#define _e_s(Tag, Type)                                                \
+    struct Tag                                                         \
+    {                                                                  \
+        using type = Type;                                             \
+                                                                       \
+        static inline auto get() -> ::reg::detail::modify_type<type>   \
+        {                                                              \
+            return _value;                                             \
+        }                                                              \
+                                                                       \
+        static inline void set(::reg::detail::modify_type<type> value) \
+        {                                                              \
+            _value = value;                                            \
+        }                                                              \
+                                                                       \
+    private:                                                           \
+        static type _value;                                            \
     }
 
-#define _e_reader(Tag, Type, Reader)                                        \
+#define _e_r(Tag, Type, Reader)                                             \
     struct Tag                                                              \
     {                                                                       \
+        using type = Type;                                                  \
+                                                                            \
         static_assert(                                                      \
             (std::is_const<Type>::value && std::is_reference<Type>::value)  \
                 || !std::is_reference<Type>::value,                         \
             "read accessor should return const reference or a value");      \
                                                                             \
-        using type = Type;                                                  \
-                                                                            \
-        static inline auto get() -> decltype(Reader()) { return Reader(); } \
+        template<typename... Args>                                          \
+        static inline auto get(Args&&... args) -> decltype(Reader(args...)) \
+        {                                                                   \
+            return Reader(::std::forward<Args>(args)...);                   \
+        }                                                                   \
     }
 
-#define _store_e(Tag, Type) \
-    Type Tag::_value {}
-
-#define _e_reader_writer(Tag, Type, Reader, Writer)                         \
+#define _e_rw(Tag, Type, Reader, Writer)                                    \
     struct Tag                                                              \
     {                                                                       \
         using type = Type;                                                  \
                                                                             \
-        static inline auto get() -> decltype(Reader()) { return Reader(); } \
-                                                                            \
-        static inline void set(Type value)                                  \
+        template<typename... Args>                                          \
+        static inline auto get(Args&&... args) -> decltype(Reader(args...)) \
         {                                                                   \
-            return Writer(std::forward<Type>(value));                       \
+            return Reader(::std::forward<Args>(args)...);                   \
+        }                                                                   \
+                                                                            \
+        template<typename... Args>                                          \
+        static inline void set(type value, Args&&... args)                  \
+        {                                                                   \
+            Writer(value, ::std::forward<Args>(args)...);                   \
         }                                                                   \
     }
 
+// store static
+#define _store_e(Tag, Type) \
+    Tag::type Tag::_value {}
+
 #define _GET_LAST_ARG(tag, arg1, arg2, arg3, arg4, ...) arg4
-#define _e_choose(...) \
-    _GET_LAST_ARG(__VA_ARGS__, _e_reader_writer, _e_reader, _e_static, )
+
+#define _e_choose(...) _GET_LAST_ARG(__VA_ARGS__, _e_rw, _e_r, _e_s, )
 
 #define _e(...) _e_choose(__VA_ARGS__)(__VA_ARGS__)
 
